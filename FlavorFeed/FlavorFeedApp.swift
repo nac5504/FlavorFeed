3282a982c15f7c1b9b5fc0bd77bc40ee323ab5ec
nac5504 <nac5504@gmail.com>
2023-09-26 17:57:56 -0400

    Initial Commit

---
+++ FlavorFeed/FlavorFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorFeedApp.swift
+//  FlavorFeed
+//
+//  Created by Nicholas Candello on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FlavorFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
